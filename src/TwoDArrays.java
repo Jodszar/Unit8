cecafe007efcc487b6756b9076af27428dd894f7
jodieazar <Jodieazar@gmail.vom>
2020-03-29 11:22:04 -0400

    Finished 2D Array Problems

---
+++ src/TwoDArrays.java
@@ -0,0 +1,22 @@
+public class TwoDArrays {
+    public static int maxValue(int[][] arr){
+        int maxnum = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            for (int x=0; x<arr[0].length; x++){
+                if(maxnum<arr[i][x])
+                    maxnum= arr[i][x];
+            }
+        }
+        return maxnum;
+    }
+    public static int sumAll(int[][] arr){
+        int total= 0;
+        for (int i= 0; i<arr.length; i++){
+            for (int x=0; x<arr[0].length; i++){
+                total+= arr[i][x];
+            }
+        }
+        return total;
+    }
+
+}
